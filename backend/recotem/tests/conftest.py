228e28d1de8f6a42ca92414a4b4bf7eadae77b4e
Tomoki Ohtsuki <tomoki.ohtsuki129@gmail.com>
2021-06-04 15:03:30 +0000

    Fix test

--- backend/recotem/tests/conftest.py
+++ backend/recotem/tests/conftest.py
@@ -6,6 +6,10 @@ from irspack.dataset import MovieLens100KDataManager
 
 from recotem.api.models import TrainingData, User
 
+# If we don't import them here, # test will now run
+# as the tasks are not registered.
+from recotem.api.tasks import run_search, start_tuning_job, train_recommender
+
 
 @pytest.fixture
 def ml100k():

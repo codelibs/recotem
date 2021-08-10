7884d5816853b4530660b9f6bf7cf5186fa00596
tohtsky <you@example.com>
2021-08-11 00:21:35 +0900

    Made mdi & roboto locally available

--- frontend/src/plugins/vuetify.ts
+++ frontend/src/plugins/vuetify.ts
@@ -1,6 +1,12 @@
 import Vue from "vue";
 import Vuetify from "vuetify/lib/framework";
+import '@mdi/font/css/materialdesignicons.css' // Ensure you are using css-loader
+
 
 Vue.use(Vuetify);
 
-export default new Vuetify({});
+export default new Vuetify({
+    icons: {
+        iconfont: 'mdi'
+    }
+});
